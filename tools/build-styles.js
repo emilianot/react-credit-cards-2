9dd473efde370fd56942432b8c5789eddf2ed26e
EmilianoT <emilianot@gmail.com>
2025-03-04 18:41:35 -0300

    build

--- tools/build-styles.js
+++ tools/build-styles.js
@@ -1,19 +1,19 @@
 /*eslint-disable no-var, vars-on-top, no-console */
 const { promisify } = require("util");
 const { exec } = require("child_process");
-
+/*
 const run = promisify(exec);
 
-run("rm -rf .tmp/")
-  .then(() => run("sass src/styles.scss .tmp/styles.css"))
+run("rem ./tmp/")
+  .then(() => run("sass src/styles.scss tmp/styles.css"))
   .then(() =>
-    run("postcss .tmp/styles.css --use autoprefixer --no-map -d .tmp/")
+    run("postcss tmp/styles.css --use autoprefixer --no-map -d tmp/")
   )
-  .then(() => run("mv .tmp/styles.css .tmp/styles-compiled.css"))
+  .then(() => run("cp ./tmp/styles.css ./tmp/styles-compiled.css"))
   // .then(() => run("mkdir -p dist/es dist/lib"))
   .then(() =>
     run(
-      "cp .tmp/styles-compiled.css dist/es/ && cp .tmp/styles-compiled.css dist/lib/"
+      "cp tmp/styles-compiled.css dist/es/ && cp tmp/styles-compiled.css dist/lib/"
     )
   )
   .then(() =>
@@ -23,3 +23,4 @@ run("rm -rf .tmp/")
   .catch((err) => {
     console.error(err);
   });
+*/
